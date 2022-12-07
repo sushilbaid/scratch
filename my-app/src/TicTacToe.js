6877145477b116b08907a169a44cfee27fc09953
ubuntu2 <sushil.baid@sacumen.com>
2022-12-07 09:38:33 +0530

    added time travel

--- my-app/src/TicTacToe.js
+++ my-app/src/TicTacToe.js
@@ -14,81 +14,39 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nextPlayer: 'X',
-            values: Array(9).fill(null),
-        };
-    }
-
     renderSquare(i, disabled) {
         return (
             <Square 
-              value={this.state.values[i]}
-              onClick={()=> this.handleClick(i)}
+              value={this.props.values[i]}
+              onClick={()=> this.props.onClick(i)}
               disabled={disabled}
             />
         );
     }
 
-    handleClick(i) {
-        console.log('click')
-        const updated = this.state.values.slice();
-        updated[i] = this.state.nextPlayer;
-        const state = this.checkGameState(updated);
-        this.setState(state);
-    }
-
     nextPlayerStatus() {
-        return 'Next Player: ' + this.state.nextPlayer;
+        return 'Next Player: ' + this.props.nextPlayer;
     }
 
     winnerStatus() {
-        return 'Winner: ' + this.state.winner;
+        return 'Winner: ' + this.props.winner;
     }
 
     gameOverStatus() {
         return 'Game Over!';
     }
 
-    checkGameState(values) {
-        const winningPositionSets = [
-            // rows
-            [0,1,2], [3,4,5], [6,7,8],
-            // columns
-            [0,3,6], [1,4,7], [2,5,8],
-            // diagonals
-            [0,4,8], [2,4,6],
-        ];
-        for (const [a,b,c] of winningPositionSets) {
-            if (values[a] && values[a] === values[b] && values[b] === values[c]) {
-                return {winner: values[a], gameOver:true, values: values};
-            }
-        }
-
-        const gameOver = !values.some(value => value == null);
-        if (gameOver) {
-            return {gameOver:gameOver, values: values};
-        } else {
-            return {
-                nextPlayer: this.state.nextPlayer === 'X' ? 'O' : 'X',
-                values: values
-            };
-        }
-    }
-
     render() {
         let status;
-        if (this.state.winner != null) 
+        if (this.props.winner != null) 
             status = this.winnerStatus();
-        else if (this.state.gameOver) 
+        else if (this.props.gameOver) 
             status = this.gameOverStatus();
         else {
             status = this.nextPlayerStatus();
         }
 
-        const d = this.state.gameOver;
+        const d = this.props.gameOver;
         return (
             <div>
                 <div className='status'>{status}</div>
@@ -113,18 +71,92 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{values: Array(9).fill(null), nextPlayer: 'X' }],
+            step: 0,
+        };
+    }
+
+    handleClick(i) {
+        console.log('click')
+        const history = this.state.history.slice(0, this.state.step + 1);
+        const len = history.length;
+        const currentState = history[len-1];
+        if (currentState.values[i]) {
+            // is already set
+            return;
+        }
+        const updated = currentState.values.slice();
+        updated[i] = currentState.nextPlayer;
+        const state = this.checkGameState(updated, currentState.nextPlayer);
+        this.setState({
+            history: history.concat(state),
+            step: this.state.step + 1,
+        });
+    }
+
+    checkGameState(values, nextPlayer) {
+        const winningPositionSets = [
+            // rows
+            [0,1,2], [3,4,5], [6,7,8],
+            // columns
+            [0,3,6], [1,4,7], [2,5,8],
+            // diagonals
+            [0,4,8], [2,4,6],
+        ];
+        for (const [a,b,c] of winningPositionSets) {
+            if (values[a] && values[a] === values[b] && values[b] === values[c]) {
+                return {winner: values[a], gameOver:true, values: values};
+            }
+        }
+
+        const gameOver = !values.some(value => value == null);
+        if (gameOver) {
+            return {gameOver:gameOver, values: values};
+        } else {
+            return {
+                nextPlayer: nextPlayer === 'X' ? 'O' : 'X',
+                values: values
+            };
+        }
+    }
+
+    jumpTo(move) {
+        this.setState({
+            step: move,
+        })
+    }
+
     render() {
+        const history = this.state.history;
+        const currentState = history[this.state.step];
+        const moves = history.map((step,move) => {
+            const desc = move ? "go to move #" + move : "go to game start"
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+        
         return (
             <div className='game'>
                 <div className="game-board">
-                    <Board />
+                    <Board 
+                     values={currentState.values}
+                     nextPlayer={currentState.nextPlayer}
+                     winner={currentState.winner}
+                     gameOver={currentState.gameOver}
+                     onClick={(i)=>this.handleClick(i)}
+                      />
                 </div>
                 <div className="game-info">
-                    <div>{}</div>
-                    <ol>{}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
-        )
+        );
     }
 }
 
